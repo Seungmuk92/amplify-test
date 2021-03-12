a07ec0524b309c60d1813b561a3ce19b5a566a15
smoh <seungmuk92@gmail.com>
2021-03-12 16:07:18 +0900

    .env test.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>AWS Amplify Test</p>
+      <p>AWS Amplify Test({process.env.REACT_APP_TEST_ENV_STRING})</p>
+      {
+        process.env.REACT_APP_AMPLIFY_ENV_STRING && 
+        <p>AWS Amplify evn value is found({process.env.REACT_APP_AMPLIFY_ENV_STRING})</p>
+      }
     </div>
   );
 }
